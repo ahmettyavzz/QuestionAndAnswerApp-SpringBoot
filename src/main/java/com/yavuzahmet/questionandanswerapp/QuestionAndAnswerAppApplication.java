a6294e7e21417c0709bdc107fa9de040b9e9fd40
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-02-05 00:52:13 +0300

    Initial commit

---
+++ src/main/java/com/yavuzahmet/questionandanswerapp/QuestionAndAnswerAppApplication.java
@@ -0,0 +1,13 @@
+package com.yavuzahmet.questionandanswerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionAndAnswerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionAndAnswerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yavuzahmet/questionandanswerapp/QuestionAndAnswerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yavuzahmet.questionandanswerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionAndAnswerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
