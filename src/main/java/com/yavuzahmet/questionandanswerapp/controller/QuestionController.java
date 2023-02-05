1a29aa4368037b17313000f118d5fe761f1fbb9b
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-02-05 21:42:50 +0300

    update controller classes

--- src/main/java/com/yavuzahmet/questionandanswerapp/controller/QuestionController.java
+++ src/main/java/com/yavuzahmet/questionandanswerapp/controller/QuestionController.java
@@ -2,17 +2,15 @@ package com.yavuzahmet.questionandanswerapp.controller;
 
 import com.yavuzahmet.questionandanswerapp.dto.QuestionDto;
 import com.yavuzahmet.questionandanswerapp.response.QuestionResponse;
-import com.yavuzahmet.questionandanswerapp.model.Question;
 import com.yavuzahmet.questionandanswerapp.service.QuestionService;
-import javax.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/v1/question")
@@ -24,17 +22,18 @@ public class QuestionController {
     }
 
     @GetMapping("/{id}")
-    public Question getQuestionById(@PathVariable String id) {
-        return questionService.getQuestionById(id);
+    public ResponseEntity<QuestionResponse> getQuestionById(@PathVariable String id) {
+        return ResponseEntity.ok(questionService.getQuestionById(id));
+    }
+
+    @GetMapping("/answer/{id}")
+    public ResponseEntity<String> answer(@PathVariable Long id,
+                                         @RequestParam Long no) {
+        return ResponseEntity.ok(questionService.answer(id, no));
     }
 
     @PostMapping
-    public Question saveQuestion(@RequestBody @Valid QuestionDto questionDto) {
-        return questionService.saveQuestion(questionDto);
-    }
-
-    @GetMapping
-    public List<QuestionResponse> getAllQuestions(){
-        return questionService.getAllQuestions();
+    public ResponseEntity<QuestionResponse> saveQuestion(@RequestBody QuestionDto dto) {
+        return ResponseEntity.ok(questionService.saveQuestion(dto));
     }
 }
--- src/main/java/com/yavuzahmet/questionandanswerapp/controller/UserController.java
+++ src/main/java/com/yavuzahmet/questionandanswerapp/controller/UserController.java
@@ -3,6 +3,7 @@ package com.yavuzahmet.questionandanswerapp.controller;
 import com.yavuzahmet.questionandanswerapp.dto.UserDto;
 import com.yavuzahmet.questionandanswerapp.response.UserResponse;
 import com.yavuzahmet.questionandanswerapp.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-private final UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
     @PostMapping
-    public UserResponse createUser(@RequestBody UserDto userDto){
-        return userService.createUser(userDto);
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.createUser(userDto));
     }
 }
 
