53ea7b3fddc403c4257f239861a57a9d65e7d9c5
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-02-05 21:40:42 +0300

    update service classes

--- src/main/java/com/yavuzahmet/questionandanswerapp/service/QuestionService.java
+++ src/main/java/com/yavuzahmet/questionandanswerapp/service/QuestionService.java
@@ -2,37 +2,64 @@ package com.yavuzahmet.questionandanswerapp.service;
 
 import com.yavuzahmet.questionandanswerapp.converter.QuestionConverter;
 import com.yavuzahmet.questionandanswerapp.dto.QuestionDto;
-import com.yavuzahmet.questionandanswerapp.response.QuestionResponse;
+import com.yavuzahmet.questionandanswerapp.exception.ErrorStatusCode;
+import com.yavuzahmet.questionandanswerapp.exception.GeneralException;
+import com.yavuzahmet.questionandanswerapp.model.Option;
 import com.yavuzahmet.questionandanswerapp.model.Question;
 import com.yavuzahmet.questionandanswerapp.repository.QuestionRepository;
+import com.yavuzahmet.questionandanswerapp.response.QuestionResponse;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class QuestionService {
     private final QuestionRepository questionRepository;
     private final QuestionConverter converter;
+    private final OptionService optionService;
 
-    public QuestionService(QuestionRepository questionRepository, QuestionConverter converter) {
+    public QuestionService(QuestionRepository questionRepository, QuestionConverter converter, OptionService optionService) {
         this.questionRepository = questionRepository;
         this.converter = converter;
+        this.optionService = optionService;
     }
 
-    public Optional<Question> getQuestionById(String id) {
-        return questionRepository.findById(Long.valueOf(id));
+    public QuestionResponse getQuestionById(String id) {
+        if (questionRepository.findById(Long.valueOf(id)).isEmpty()) {
+            throw new GeneralException(ErrorStatusCode.INVALID_QUESTION_ID);
+        }
+        var optionList = optionService.getOptionsByQuestionId(Long.valueOf(id));
+        var data = questionRepository.findById(Long.valueOf(id)).get();
+        data.setOptions(optionList);
+        return converter.toResponse(data);
     }
 
-    public Question saveQuestion(QuestionDto questionDto) {
-        var kay= converter.toEntity(questionDto);
+    public QuestionResponse saveQuestion(QuestionDto dto) {
 
+        Question forSaveQuestion = new Question();
+        forSaveQuestion.setHead(dto.getHead());
+        forSaveQuestion.setAnswerNo(dto.getAnswerNo());
 
-        return questionRepository.save(kay);
+        List<Option> options = dto.getOptions();
+        for (Option option : options) {
+            option.setQuestion(forSaveQuestion);
+        }
+        forSaveQuestion.setOptions(options);
+
+        questionRepository.save(forSaveQuestion);
+
+        return converter.toResponse(forSaveQuestion);
     }
 
-    public List<QuestionResponse> getAllQuestions() {
-        return questionRepository.findAll().stream().map(converter::toResource).collect(Collectors.toList());
+    public String answer(Long questionId, Long optionNo) {
+        var realAnswerNo = questionRepository.findById(questionId).orElseThrow(() -> new GeneralException(ErrorStatusCode.USER_NOT_FOUND))
+                .getAnswerNo();
+
+        var option = optionService.getOptionsByQuestionId(questionId).stream().filter(o -> o.getOptionNo() == optionNo).toList();
+
+        if (realAnswerNo == optionNo) {
+            return "Correct! " + " : " + option.get(0).getHead();
+        }
+        return "Wrong! " + " : " + option.get(0).getHead();
     }
 }
