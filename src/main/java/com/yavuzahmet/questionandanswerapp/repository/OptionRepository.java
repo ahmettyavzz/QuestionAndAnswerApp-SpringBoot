5fcafe045a2e3e30868e9d7648a6bc68b1ea5cef
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-02-05 21:40:13 +0300

    add findByQuestionId method in OptionRepository

--- src/main/java/com/yavuzahmet/questionandanswerapp/repository/OptionRepository.java
+++ src/main/java/com/yavuzahmet/questionandanswerapp/repository/OptionRepository.java
@@ -3,5 +3,8 @@ package com.yavuzahmet.questionandanswerapp.repository;
 import com.yavuzahmet.questionandanswerapp.model.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OptionRepository extends JpaRepository<Option, Long> {
+    List<Option> findByQuestionId(Long questionId);
 }
